9e522f94cd8c7239396e842469ccb9f3a9ee0318
AbhishekBhati108 <abhishek10062000@gmail.com>
2023-12-16 11:01:12 +0530

    ep-2

--- App.js
+++ App.js
@@ -1,4 +1,11 @@
-const heading = React.createElement("h1", {}, "It is react element");
+import React from "react";
+import ReactDOM from "react-dom";
+
+const heading = React.createElement(
+  "h1",
+  {},
+  React.createElement("h2", {}, "This is nested element")
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
