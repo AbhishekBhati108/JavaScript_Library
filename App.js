fc7b2b135d60fffa3d1300c3dec9410c48b0bf21
AbhishekBhati108 <abhishek10062000@gmail.com>
2023-12-17 11:28:07 +0530

    ep-3

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const heading = React.createElement(
+// React.createContext => React element - JS Object => HTML Element(render)
+/*const heading = React.createElement(
   "h1",
-  {},
+  { id: "heading" },
   React.createElement("h2", {}, "This is nested element")
+);*/
+
+// JSX => React.createContext => React element - JS Object => HTML Element(render)
+
+const Title = () => (
+  <h1 className="title" tabIndex={1}>
+    This is React App.
+  </h1>
 );
+
+// React functional compontent
+const HeadingCompontent = () => {
+  return (
+    <div id="container">
+      <Title />
+      <h1 className="heading">This is React functional compontent.</h1>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(<HeadingCompontent />);
