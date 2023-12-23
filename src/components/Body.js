0e61a3a018537ca80f949c7d7e79935a969d3d93
AbhishekBhati108 <abhishek10062000@gmail.com>
2023-12-23 17:44:20 +0530

    ep-6.1

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,27 @@
 import RestaurandCard from "./RestaurandCard";
 import resList from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Body = () => {
-  const [listOfRestaurant, setListOfRestaurant] = useState(resList);
+  const [listOfRestaurant, setListOfRestaurant] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.4743879&lng=77.50399039999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+
+    console.log(
+      json.data.cards[5].card.card.gridElements.infoWithStyle.restaurants
+    );
+    setListOfRestaurant(
+      json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
 
   return (
     <div className="body">
@@ -12,7 +30,7 @@ const Body = () => {
           className="filter-btn"
           onClick={() => {
             const filterRes = listOfRestaurant.filter(
-              (res) => res.data.avgRating > 4
+              (res) => res.info.avgRating > 4
             );
             setListOfRestaurant(filterRes);
           }}
@@ -22,7 +40,7 @@ const Body = () => {
       </div>
       <div className="res-container">
         {listOfRestaurant.map((restaurant) => (
-          <RestaurandCard key={restaurant.data.id} resData={restaurant} />
+          <RestaurandCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
     </div>
--- src/components/RestaurandCard.js
+++ src/components/RestaurandCard.js
@@ -2,8 +2,7 @@ import { CDN_URL } from "../utils/constants";
 
 const RestaurandCard = (props) => {
   const { resData } = props;
-  const { name, cuisines, avgRating, minDeliveryTime, cloudinaryImageId } =
-    resData?.data;
+  const { name, cuisines, avgRating, sla, cloudinaryImageId } = resData?.info;
   return (
     <div className="res-card">
       <img
@@ -12,9 +11,9 @@ const RestaurandCard = (props) => {
         src={CDN_URL + cloudinaryImageId}
       ></img>
       <h5>{name}</h5>
+      <h5>{avgRating} stars</h5>
       <h5>{cuisines.join(", ")}</h5>
-      <h5>{avgRating}</h5>
-      <h5>{minDeliveryTime} minutes</h5>
+      <h5>{sla.slaString}</h5>
     </div>
   );
 };
